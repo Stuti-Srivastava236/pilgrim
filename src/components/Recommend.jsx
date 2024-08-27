de88b4b82552499e358a016929934ad1e91b96b5
Stuti Srivastava <stutisrivastava024@gmail.com>
2024-08-27 07:33:54 +0530

    first commit

---
+++ src/components/Recommend.jsx
@@ -0,0 +1,196 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import h2 from "../assets/h2.jpeg";
+import nidhi from "../assets/nidhi.jpeg";
+import h4 from "../assets/h4.jpg";
+import h6 from "../assets/h6.jpeg";
+import hero from "../assets/hero.jpeg";
+import image from "../assets/image.png";
+import info1 from "../assets/info1.png";
+import info2 from "../assets/info2.png";
+import info3 from "../assets/info3.png";
+
+export default function Recommend() {
+  const data = [
+    {
+      image: h2,
+      title: "Kedarnath",
+      subTitle: "Kedarnath being knoen for its hard to cover",
+      cost: "38,800",
+      duration: "Approx 2 night trip",
+    },
+    {
+      image: nidhi,
+      title: "Vrindavan",
+      subTitle: "The land of ancient india is still the same despiter",
+      cost: "54,200",
+      duration: "Approx 2 night trip",
+    },
+    {
+      image: h4,
+      title: "Haridwar",
+      subTitle: "Place showing the sangam of different",
+      cost: "45,500",
+      duration: "Approx 2 night trip",
+    },
+    {
+      image: h6,
+      title: "Varanasi",
+      subTitle: "the most ancient city of not only the country but",
+      cost: "24,100",
+      duration: "Approx 1 night trip",
+    },
+    {
+      image: hero,
+      title: " Varanasi",
+      subTitle: "Varanasi, the city of moksha, the city of",
+      cost: "95,400",
+      duration: "Approx 2 night 2 day trip",
+    },
+    {
+      image: image,
+      title: "Amritsar",
+      subTitle: "Konwn for the beauty of simplicity and peace",
+      cost: "38,800",
+      duration: "Approx 3 night 2 day trip",
+    },
+  ];
+
+  const packages = [
+    "The Weekend Break",
+    "The Package Holiday",
+    "The Group Tour",
+    "Long Term Slow Travel",
+  ];
+
+  const [active, setActive] = useState(1);
+  return (
+    <Section id="recommend">
+      <div className="title">
+        <h2>Recommended Destinations</h2>
+      </div>
+      <div className="packages">
+        <ul>
+          {packages.map((pkg, index) => {
+            return (
+              <li
+                className={active === index + 1 ? "active" : ""}
+                onClick={() => setActive(index + 1)}
+              >
+                {pkg}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="destinations">
+        {data.map((destination) => {
+          return (
+            <div className="destination">
+              <img src={destination.image} alt="" />
+              <h3>{destination.title}</h3>
+              <p>{destination.subTitle}</p>
+              <div className="info">
+                <div className="services">
+                  <img src={info1} alt="" />
+                  <img src={info2} alt="" />
+                  <img src={info3} alt="" />
+                </div>
+                <h4>{destination.cost}</h4>
+              </div>
+              <div className="distance">
+                <span>1000 Kms</span>
+                <span>{destination.duration}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Section>
+  );
+}
+
+const Section = styled.section`
+  padding: 2rem 0;
+  .title {
+    text-align: center;
+  }
+  .packages {
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0;
+    ul {
+      display: flex;
+      list-style-type: none;
+      width: max-content;
+      li {
+        padding: 1rem 2rem;
+        border-bottom: 0.1rem solid black;
+      }
+      .active {
+        border-bottom: 0.5rem solid #8338ec;
+      }
+    }
+  }
+  .destinations {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+    padding: 0 3rem;
+    .destination {
+      padding: 1rem;
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      background-color: #8338ec14;
+      border-radius: 1rem;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        transform: translateX(0.4rem) translateY(-1rem);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      }
+      img {
+        width: 100%;
+      }
+      .info {
+        display: flex;
+        align-items: center;
+        .services {
+          display: flex;
+          gap: 0.3rem;
+          img {
+            border-radius: 1rem;
+            background-color: #4d2ddb84;
+            width: 2rem;
+            /* padding: 1rem; */
+            padding: 0.3rem 0.4rem;
+          }
+        }
+        display: flex;
+        justify-content: space-between;
+      }
+      .distance {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 768px) {
+    .packages {
+      ul {
+        li {
+          padding: 0 0.5rem;
+          font-size: 2vh;
+          padding-bottom: 1rem;
+        }
+        .active {
+          border-bottom-width: 0.3rem;
+        }
+      }
+    }
+    .destinations {
+      grid-template-columns: 1fr;
+      padding: 0;
+    }
+  }
+`;
